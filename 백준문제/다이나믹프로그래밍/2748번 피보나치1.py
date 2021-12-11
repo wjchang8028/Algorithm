c3eba776bf1bc6a029f879a38a976fffc9422fed
eukjin <wjchang8028@naver.com>
2021-12-11 18:01:05 +0900

    백준문제풀이(21/12/11)

---
+++ 백준문제/다이나믹프로그래밍/2748번 피보나치1.py
@@ -0,0 +1,18 @@
+#n번째 피보나치수 구하기
+
+d = [0] * 100
+
+def fibo(x):
+
+    if x == 1 or x == 2:
+        return 1
+
+    if d[x] != 0:
+        return d[x]
+    
+    d[x] = fibo(x-1) + fibo(x-2)
+    
+    return d[x]
+
+n = int(input())
+print(fibo(n))
