2b3852da6fec5ab1460f81216b4d762821f7fb4d
eukjin <wjchang8028@naver.com>
2021-12-14 13:52:38 +0900

    기본개념(21/12/14)

---
+++ 기본문법수업/내장함수사용법/Counter().py
@@ -0,0 +1,19 @@
+#데이터의 갯수를 셀때 유용함
+
+from collections import Counter
+
+word = "Hello World!"
+c = Counter(word)
+array = list(c.keys())
+print(array)
+
+#Counter마크를 제거한 리스트(빈도수에따라서 자동정렬)
+mostcounter = Counter(word).most_common()
+print(mostcounter)
+
+for i in range(len(mostcounter)):
+    print(mostcounter[i][0])
+
+#인자(2) 갯수 이상의 문자들만 출력
+mostcounter = Counter(word).most_common(2)
+print(mostcounter)
