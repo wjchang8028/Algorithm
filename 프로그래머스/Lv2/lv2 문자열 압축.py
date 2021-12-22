3c072dd8220f7dcb635fd121bd2ab7fad6f5ec47
eukjin <wjchang8028@naver.com>
2021-12-22 11:05:00 +0900

    프로그래머스(21/12/22)

---
+++ 프로그래머스/Lv2/lv2 문자열 압축.py
@@ -0,0 +1,34 @@
+def solution(s):
+
+    result = []
+    if len(s) == 1:
+        return 1
+    for i in range(1,(len(s) // 2) + 1):
+        b = ''
+        cnt = 1
+        temp = s[:i]
+
+        for j in range(i,len(s),i):
+            if temp == s[j:i+j]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    b = b + str(cnt) + temp
+                else:
+                    b = b + temp
+                temp = s[j:j+i]
+                cnt = 1
+        if cnt != 1:
+            b = b + str(cnt) + temp
+        else:
+            b = b + temp
+
+        result.append(len(b))
+        answer = min(result)
+    print(result)
+    print(answer)
+    return answer
+
+s = "aabbaccc"
+
+solution(s)
