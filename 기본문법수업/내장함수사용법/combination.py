a54d71276ec1fad81ab3765e9e23bbaad8145b51
eukjin <wjchang8028@naver.com>
2021-12-27 15:00:48 +0900

    콤비네이션 개념추가

--- 기본문법수업/내장함수사용법/combination.py
+++ 기본문법수업/내장함수사용법/combination.py
@@ -4,6 +4,7 @@
 from itertools import permutations
 from itertools import combinations
 from itertools import product
+from itertools import chain
 
 item = [1,2,3,4,5]
 items = [[1,2,3],['a','b','c'],[4,5,6]]
@@ -14,3 +15,7 @@ print(list(combinations(item,2)))
 
 #2개 이상 리스트조합
 print(list(product(*items)))
+
+#리스트의 인자들을 전부 조합하는 경우(각 숫자를 뽑아서 만들수 있는 경우의 수 )
+print(list(chain.from_iterable(permutations(item,i)for i in range(len(item)+1)))) 
+print(list(chain.from_iterable(combinations(item,i)for i in range(len(item)+1)))) 
