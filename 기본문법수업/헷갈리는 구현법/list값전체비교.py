a36f84ead08970b8fb69bd45251fbb0ec44d39c9
eukjin <wjchang8028@naver.com>
2021-12-30 16:31:36 +0900

    (21/12/30)

--- 기본문법수업/헷갈리는 구현법/list값전체비교.py
+++ 기본문법수업/헷갈리는 구현법/list값전체비교.py
@@ -1,8 +1,12 @@
 # 리스트에서 처음값과 뒤에있는 수들 비교할때.
 
 array = [1,3,4,2,5]
-temp = []
-for i in range(len(array)):
-    if array[0] < array[i]:
-        print(array[i])
+temp = [6,7,8]
+temp2 = []
+for i in array:
+    if temp: #if 리스트 : 은 값이 있을때만 돌고 없을땐 돌지않음
+        print("temp내에 원소가 있을때",i)
+    if temp2:
+        print("empty",i)
+
         
--- 프로그래머스/Lv2/lv2.트럭.py
+++ 프로그래머스/Lv2/lv2.트럭.py
@@ -1,20 +1,43 @@
+ from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0 
     bridge = []
 
     bridge = [0] * bridge_length
-    
+
     while len(bridge):
         bridge.pop(0)
-
+        answer += 1
         if truck_weights:
-            print(truck_weights[0],truck_weights)
             if sum(bridge) + truck_weights[0] <= weight:
                 bridge.append(truck_weights.pop(0))
             else:
                 bridge.append(0)
-        print(bridge,truck_weights)
-
+        # print(bridge,truck_weights)
+    print(answer)
     return answer
 
-solution(2,10,[7])
+def other_solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridgeWeight = 0 # 다리 위의 트럭 무게
+    waiting = deque(truck_weights) # 대기 트럭 큐
+    bridge = deque([0 for _ in range(bridge_length)]) # 다리 길이만큼 0으로 채운다.
+    
+    while len(waiting) or bridgeWeight > 0: # 대기 트럭이 있거나 이동 중인 트럭이 있는 동안 반복
+        removedTruck = bridge.popleft() # 다리에서 하나 제거
+        bridgeWeight -= removedTruck
+        
+        if len(waiting) and bridgeWeight + waiting[0] <= weight: # 새 트럭이 다리에 올라갈 수 있으면
+            newTruck = waiting.popleft()
+            bridgeWeight += newTruck
+            
+            bridge.append(newTruck) # 대기 트럭에서 하나 빼서 다리에 올림
+            
+        else: # 새 트럭이 다리에 올라갈 수 없으면
+            bridge.append(0) # 오른쪽에 0을 삽입해서 다리 길이 유지
+            
+        time += 1
+    return time
+
+other_solution(2,10,[7,4,5,6])
