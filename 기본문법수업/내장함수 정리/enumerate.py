c8b89c75c137d44b3db69da0db25910378028c9b
eukjin <wjchang8028@naver.com>
2021-12-31 12:23:05 +0900

    .

---
+++ 기본문법수업/내장함수 정리/enumerate.py
@@ -0,0 +1,11 @@
+t = [1,3,5,7,9,11,13,15]
+
+#enumerate를 쓰면 index 와 value로 묶은 튜플형태로 받을 수 있다. ex) a = 1 , b = 3 , c = 5 ... 순서정해놓을때
+# 순서와 리스트의 값을 전달하는 역할 (enumerate = 열거하다)
+for i in enumerate(t):
+    print(i)
+
+
+#시작 인덱스바꾸기(1부터시작)
+for i,num in enumerate(t,start = 1):
+    print(i,num)
