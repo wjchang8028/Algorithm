4bf15b7eedc1b1dfc7c8b5baee89f4ec8f9c2bf3
eukjin <wjchang8028@naver.com>
2022-01-06 14:28:22 +0900

    구현

--- 알고리즘책/4장 구현테스트/구현다시풀기/시각.py
+++ 알고리즘책/4장 구현테스트/구현다시풀기/시각.py
@@ -2,6 +2,15 @@
 
 def solution(n):
     answer = 0
-
+    time = ''
+    for hour in range(n+1):
+        for min in range(0,60):
+            for sec in range(0,60):
+                time = str(hour)+str(min)+str(sec)
+                
+                if '3' in time:
+                    answer += 1
+    print(answer)
     return answer
 
+solution(5) 
