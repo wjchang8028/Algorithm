08b5f4590bee3644496305a44d34d41a33d2e62e
eukjin <wjchang8028@naver.com>
2022-01-07 15:26:24 +0900

    프로그래머스

---
+++ 프로그래머스/Lv2/lv2 짝지어 제거하기.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = -1
+    stack = []
+
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+        
+        print(stack)
+
+    if len(stack) == 0:
+        answer = 1
+    else:
+        answer = 0
+
+    print(answer)
+
+    return answer
+
+s = 'cdcd'
+solution(s)
