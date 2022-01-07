d3470d94005986b647c7257ec30a9adc85c25056
eukjin <wjchang8028@naver.com>
2022-01-07 16:59:29 +0900

    풀이

--- 프로그래머스/Lv1/lv1 키패드.py
+++ 프로그래머스/Lv1/lv1 키패드.py
@@ -65,4 +65,7 @@ def solution(numbers, hand):
     return answer
 
 solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],	"right")
+print()
 solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],	"right")
+print()
+solution([1,3,2],"right")
