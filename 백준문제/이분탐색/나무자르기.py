01ae3cb42a28b2be2f013243480a9c8bb6f8b627
eukjin <wjchang8028@naver.com>
2022-01-12 12:02:00 +0900

    떡볶이와 같은문제

---
+++ 백준문제/이분탐색/나무자르기.py
@@ -0,0 +1,26 @@
+
+def solution(array,m,start,end):
+    
+    while (start <= end):
+        mid = (start + end) // 2 # 중간
+
+        total = 0
+
+        for i in array:
+            if i > mid:
+                total += i-mid
+
+        if total < m:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+
+    print(result)
+    return result
+
+n,m = map(int,input().split())
+
+array = list(map(int,input().split()))
+
+solution(array,m,0,max(array))
