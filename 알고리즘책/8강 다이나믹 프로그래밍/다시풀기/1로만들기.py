9c412de45253b2d3cdf45980be413f0d3e437f07
eukjin <wjchang8028@naver.com>
2022-01-13 12:58:24 +0900

    .

---
+++ 알고리즘책/8강 다이나믹 프로그래밍/다시풀기/1로만들기.py
@@ -0,0 +1,20 @@
+def solution(n):
+    d = [0] * 30
+
+    for i in range(2,n+1):
+        d[i] = d[i-1] + 1
+
+        if i % 2 == 0:
+            d[i] = min(d[i],d[i // 2] + 1)
+        
+        if i % 3 == 0:
+            d[i] = min(d[i],d[i // 3] + 1)
+        
+        if i % 5 == 0:
+            d[i] = min(d[i],d[i // 5] + 1)
+    print(d)
+    print(d[n])
+    return d[n]
+
+n = 26
+solution(n)
--- 알고리즘책/8강 다이나믹 프로그래밍/다시풀기/개미전사창고털기.py
+++ 알고리즘책/8강 다이나믹 프로그래밍/다시풀기/개미전사창고털기.py
@@ -11,7 +11,7 @@ def solution(array,n):
     d[1] = max(array[0], array[1])
 
     for i in range(2,n):
-        d[i] = max(d[i-1],d[i-2]+array[i])
+        d[i] = max(d[i-1],d[i-2]+array[i]) #amx(직전 창고까지의 총 식량, 두번째 전까지의 총 식량 + 현재창고의 식량)
 
     print(d[n-1])
     return d[n-1]
