5c65bdca7ede2d1a5ea01dc68e14ff7969dcaa4c
eukjin <wjchang8028@naver.com>
2022-01-18 14:01:38 +0900

    전보

--- 알고리즘책/9강 최단경로/다시풀기/전보.py
+++ 알고리즘책/9강 최단경로/다시풀기/전보.py
@@ -1,59 +1,54 @@
-import heapq
 import sys
+import heapq
 
-input = sys.stdin.readline
+n,m,start = map(int,sys.stdin.readline().split())
 
 INF = int(1e9)
-
-n,m,start = map(int,input().split()) #도시갯수,통로갯수,메세지보내는 도시
-
 graph = [[] for _ in range(n+1)]
-
-visited = [False] * (n+1)
-
+visited = [False] * (n+1) 
 distance = [INF] * (n+1)
 
 for _ in range(m):
-    x,y,z = map(int,input().split())
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
 
-    graph[x].append((y,z))
+print(graph)
 
 def dijkstra(start):
     q = []
     heapq.heappush(q,(0,start))
-
     distance[start] = 0
 
     while q:
-        dist, now = heapq.heappop(q)
+        dist,now = heapq.heappop(q)
+
         if distance[now] < dist:
             continue
 
         for i in graph[now]:
             cost = dist + i[1]
-
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q,(cost,i[0]))
 
 dijkstra(start)
 
-count = 0
-max_distance = 0
-
+city = 0
+total_time = 0
 for i in distance:
-    if i != INF:
-        count += 1
-        max_distance = max(max_distance,i)
 
-print(count - 1, max_distance)
-print(graph)
+    if i == INF:
+        pass
+    else:
+        city += 1
+        total_time = max(total_time,i)
+print(city - 1)
+print(total_time)
 
-# 입력예제
-
-# 3 2 1
+# 4 4 1
 # 1 2 4
 # 1 3 2
+# 2 3 5
+# 3 4 8
 
-# 결과 : 2 4
-
+# 3 , 10
