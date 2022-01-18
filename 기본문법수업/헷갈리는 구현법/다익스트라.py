7f57f6a5571b1547a217ec7bcb91ef618ecca49a
eukjin <wjchang8028@naver.com>
2022-01-18 12:33:30 +0900

    다익스트라

---
+++ 기본문법수업/헷갈리는 구현법/다익스트라.py
@@ -0,0 +1,54 @@
+# 출발노드 설정
+# 최단거리 테이블 초기화
+# 방문하지 않은 노드중 최단거리가 가장 짧은 노드 선택
+# 해당 노드를 거쳐 다른 노드로 가는 비용 계산하여 최단거리 테이블 갱신
+# 3번 4번 반복
+
+# 시간 복잡도 O(v2) => 노드의 갯수가 5000개 이하라면 가능. 
+ 
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n,m = map(int,input().split())
+start = int(input())
+
+graph = [[] for i in range(n+1)]
+visited = [False] * (n+1)
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1,n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    
+    for i in range(n-1):
+        now = get_smallest_node()
+        visited[now] = True
+
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1,n+1):
+    if distance[i] == INF:
+        print("도달할 수 없음")
+    else:
+        print(distance[i])
