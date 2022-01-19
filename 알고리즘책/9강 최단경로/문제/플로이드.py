fd74c38e07c4e40b9ea41af26d7f87341c86f4f7
eukjin <wjchang8028@naver.com>
2022-01-19 12:56:56 +0900

    문제풀이

---
+++ 알고리즘책/9강 최단경로/문제/플로이드.py
@@ -0,0 +1,30 @@
+
+n = int(input()) # 도시 수(노드)
+m = int(input()) # 버스 수(간선)
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+graph2 = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(n+1):
+    for b in range(n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a,b,c = map(int,input().split()) # a : 시작도시 b:도착도시 c:비용
+    graph[a][b] = min(graph[a][b],c) # 겹치는 구간은 더 작은 비용의 버스를 이용할 것
+
+
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+print(graph)
+
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        print(graph[a][b],end=" ")
+    print()
+
+
+
