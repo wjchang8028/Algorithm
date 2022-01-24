01645a21a92363b3b8719d9712e264febe8ab008
eukjin <wjchang8028@naver.com>
2022-01-24 09:41:14 +0900

    다이나믹

---
+++ 알고리즘책/8강 다이나믹 프로그래밍/문제/편집거리.py
@@ -0,0 +1,27 @@
+
+A = input()
+B = input()
+
+def edit_dist(str1,str2):
+    n = len(str1)
+    m = len(str2)
+
+    dp = [[0] * (m+1) for _ in range(n+1)]
+
+    for i in range(1,n+1):
+        dp[i][0] = i
+    for j in range(1,m+1):
+        dp[0][j] = j
+
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            #문자가 같으면 왼쪽위 수를 그대로 대입
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            
+            #문자가 다르다면 3가지 경우 최솟값
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) #삽입(왼쪽) , 삭제(위쪽) , 교체(왼쪽위) 중에서 최소비용 찾아 대입
+    return dp[n][m]
+
+print(edit_dist(A,B))
