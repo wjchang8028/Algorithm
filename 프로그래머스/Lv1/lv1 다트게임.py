0b52931e3dad37688b3e752d61760246afa7528f
eukjin <wjchang8028@naver.com>
2022-02-04 14:27:01 +0900

    문제

---
+++ 프로그래머스/Lv1/lv1 다트게임.py
@@ -0,0 +1,16 @@
+def solution(dartResult):
+    answer = 0
+    imsi = 0
+    temp = list(dartResult)
+    temp2=[]
+    print(temp)
+    for i in temp:
+        temp2.append(ord(i))
+
+    print(temp2)
+    
+    
+    print(answer)
+    return answer
+
+solution("1S2D#3T")
