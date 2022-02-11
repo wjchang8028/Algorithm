27f0c80a5de295334a784b40336dbdb09e900e0d
eukjin <wjchang8028@naver.com>
2022-02-11 13:48:19 +0900

    문제

--- 프로그래머스/Lv1/lv1 같은 숫자는 싫어.py
+++ 프로그래머스/Lv1/lv1 같은 숫자는 싫어.py
@@ -1,6 +1,13 @@
 def solution(arr):
     answer = []
-    
+    a = arr.pop(0)
+    answer.append(a)
+
+    for i in arr:
+        if answer[-1] != i:
+            answer.append(i)
+
+    print(answer)
     return answer
 
 arr = [1,1,1,3,3,0,1,1,1]
