ee4f2ac653e31c935b630819fcc5aafb4be7a2a5
eukjin <wjchang8028@naver.com>
2022-02-27 22:16:04 +0900

    문제풀이

--- 프로그래머스/Lv1/lv1 문자열 내맘대로 정렬하기.py
+++ 프로그래머스/Lv1/lv1 문자열 내맘대로 정렬하기.py
@@ -1,9 +1,11 @@
 def solution(strings, n):
     answer = []
-    
+
+    answer = sorted(sorted(strings),key=lambda x:x[n])
+    print(answer)
     return answer
 
 
-strings = ["abce", "abcd", "cdx"]
+strings = ["abce", "abcd", "cdx","abcf"]
 n = 2
 solution(strings,n)
