8f800293c3188dd839aa9a20b4a61a85c0381a2d
eukjin <wjchang8028@naver.com>
2022-02-27 23:21:23 +0900

    count함수 사용법

---
+++ 기본문법수업/내장함수 정리/count 함수 사용법.py
@@ -0,0 +1,12 @@
+
+arr = [1,2,3,4,4,4,5,6,7]
+print(arr.count(4)) # 리스트 내에서 4찾기 (갯수반환)
+
+arr2 = ['a','a','b','c','d','e']
+print(arr2.count('a'))  # 리스트 내에서 a찾기 (갯수반환)
+
+strings = "aabc"
+print(strings.count('a'))  # 문자열 내에서 a찾기 (갯수반환)
+
+strings2 = "111123213"
+print(strings2.count('1'))  # 문자열 내에서 5찾기 (갯수반환)
