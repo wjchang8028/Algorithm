f9a136fc665c38efa385c6851780117b1073c53d
eukjin <wjchang8028@naver.com>
2022-03-02 14:50:48 +0900

    문제풀이

--- 프로그래머스/Lv2/lv2 영어끝말잇기.py
+++ 프로그래머스/Lv2/lv2 영어끝말잇기.py
@@ -1,11 +1,16 @@
 def solution(n, words):
-    answer = []
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i] :
+            return [(i%n)+1, (i//n)+1]
+    else:
+        return [0,0]
 
 n = 3
-words= ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+words= ["tank", "kick", "know", "wheel", "land", "dream", "mimik", "kick", "kisi"]
+# words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+solution(n,words)
+
+n = 2
+words = ["hello", "one", "even", "never", "now", "world", "draw"]
+words = ["hello", "one", "iron", "never", "rw", "world", "draw"]
 solution(n,words)
