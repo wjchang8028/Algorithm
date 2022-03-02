ed8b8aa34eacd450292855fd01562fb1e7255c1d
eukjin <wjchang8028@naver.com>
2022-03-02 13:22:14 +0900

    문제풀이

---
+++ 프로그래머스/Lv2/lv2 영어끝말잇기.py
@@ -0,0 +1,11 @@
+def solution(n, words):
+    answer = []
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
+
+n = 3
+words= ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+solution(n,words)
