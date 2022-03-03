839b25cd0fdcf547b9043c96e585521ac46d95d9
eukjin <wjchang8028@naver.com>
2022-03-03 19:13:53 +0900

    문제

---
+++ 프로그래머스/Lv2/lv2 튜플.py
@@ -0,0 +1,30 @@
+import re
+from typing import Counter
+def solution(s):
+    answer = []
+    arr=[]
+
+   
+    for i in re.split('[{,}]',s):
+        if i =="":
+            pass
+        else:
+            arr.append(int(i))
+
+    print(arr)
+
+    c = Counter(arr).most_common()
+    print(c)
+
+    for i in c:
+        answer.append(i[0])
+
+    print(answer)
+
+    return answer
+
+s= "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+solution(s)
+
+s = "{{20,111},{111}}"
+solution(s)
