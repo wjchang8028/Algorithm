11304597f8b9f8c05051cca9a98ca272a190f68a
eukjin <wjchang8028@naver.com>
2022-03-10 14:10:32 +0900

    풀이

--- 프로그래머스/Lv2/테스트/lv2 테스트7.py
+++ 프로그래머스/Lv2/테스트/lv2 테스트7.py
@@ -3,10 +3,9 @@ def IsPrime(num):
     if num == 0 or num == 1:
         return False
 
-    for i in range(2,num):
+    for i in range(2,num): 
         if num % i == 0:
             return False
-
     return True
 
 def solution(numbers):
