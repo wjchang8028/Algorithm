b9677bc81c5e938ef5f64ca5d7d1689189702bd7
eukjin <wjchang8028@naver.com>
2022-03-10 14:06:41 +0900

    풀이

--- 프로그래머스/Lv2/테스트/lv2 테스트7.py
+++ 프로그래머스/Lv2/테스트/lv2 테스트7.py
@@ -1,17 +1,33 @@
 from itertools import combinations, permutations, product
+def IsPrime(num):
+    if num == 0 or num == 1:
+        return False
 
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+
+    return True
 
 def solution(numbers):
     lst = []
     answer = 0
     temp = ''
-    for i in range(1,len(numbers)+1):
-        temp += list(''.join, permutations(numbers,i))
-        
-        print(temp)
+    a = set() 
+    for i in range(len(numbers)):
+        a |= set(map(int,map("".join,permutations(list(numbers), i+1 )))) #|= or연산자를 하고 난 값으로 새로이 update한다는 의미
+    arr = list(a)
+    arr.sort()
+    print(arr)
 
-
-
+    cnt = 0
+    for i in arr:
+        if IsPrime(i):
+            cnt += 1
+        else:
+            pass
+    
+    print(cnt)
 
     return answer
 
