775bd0be01e5a43219883aad46274a8842ea114c
eukjin <wjchang8028@naver.com>
2022-03-10 14:21:53 +0900

    소수

--- 기본문법수업/헷갈리는 구현법/소수판별.py
+++ 기본문법수업/헷갈리는 구현법/소수판별.py
@@ -8,5 +8,21 @@ def IsPrime(num):
 
     return True # 그 외에는 전부 소수
 
-print(IsPrime(17))
-print(IsPrime(6))
+print(IsPrime(17)) # True
+print(IsPrime(6)) # False
+
+def eratostenes(n): # 에라토스테네스의 체(시간효율성 업)
+    answer = 0
+    temp = []
+    sosu = [False,False]+[True] * (n-1)
+
+    for i in range(2,n+1):
+        if sosu[i]:
+            temp.append(i)
+            for j in range(2*i,n+1,i):
+                sosu[j] = False
+    print(sosu)
+    return answer
+
+eratostenes(17)
+# [False, False, True, True, False, True, False, True, False, False, False, True, False, True, False, False, False, True]
