baa6c6c0db369404b8fad16a2568a7155cc04758
eukjin <wjchang8028@naver.com>
2022-03-15 19:02:06 +0900

    문제

--- 프로그래머스/Lv2/테스트/lv2 테스트9.py
+++ 프로그래머스/Lv2/테스트/lv2 테스트9.py
@@ -7,7 +7,8 @@ def solution(s):
     cnt = 0
     while cnt != len(s):
         stack = []
-        s = s[1:len(s)] + s[0]
+        s = s[1:len(s)] + s[0] #리스트 회전
+        print(s)
         answer_s = True
 
         for c in s:
@@ -18,6 +19,7 @@ def solution(s):
                     answer_s = False
                     break
                 if dic[c] == stack[len(stack) - 1]:
+                    print("c",c,"dic[c]:",dic[c])
                     stack.pop()
                 else:
                     answer_s = False
