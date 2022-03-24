2021a8ca17e343ffac7009b970e4adb36bec8755
eukjin <wjchang8028@naver.com>
2022-03-24 15:27:38 +0900

    .

--- 프로그래머스/Lv2/테스트/lv3 테스트1.py
+++ 프로그래머스/Lv2/테스트/lv3 테스트1.py
@@ -1,13 +1,37 @@
+
+
 def solution(N, number):
-    answer = 0
-    cal = ['+','-','//','*']
+    possible_set = [0,[N]]
+    if N == number:
+        return 1
 
+    for i in range(2,9):
+        case_set = []
+        basic_num = int(str(N) * i)
+        case_set.append(basic_num)
+        print(case_set)
 
+        for i_half in range(1, i//2+1):
+            for x in possible_set[i_half]:
+                for y in possible_set[i-i_half]:
 
+                    case_set.append(x+y)
+                    case_set.append(x-y)
+                    case_set.append(x*y)
+                    case_set.append(y-x)
 
-    return answer
+                    if y != 0:
+                        case_set.append(x/y)
+                    if x != 0:
+                        case_set.append(y/x)
+            if number in case_set:
+                return i
+            possible_set.append(case_set)
+
+    return -1
 
 solution(5,12)
+solution(2,11)
 
 def solution2(triangle):
     answer = 0
@@ -25,4 +49,4 @@ def solution2(triangle):
     print(answer)
     return answer
 
-solution2([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
+# solution2([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
