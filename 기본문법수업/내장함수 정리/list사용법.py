60f7f824b3dda3e132edcfc2a83195493475eb11
eukjin <wjchang8028@naver.com>
2022-03-26 14:08:13 +0900

    list

--- 기본문법수업/내장함수 정리/list사용법.py
+++ 기본문법수업/내장함수 정리/list사용법.py
@@ -3,8 +3,9 @@ array = [1,2,3,1,2,3,4]
 delarr = [1,2,2]
 #예상값 = [3,1,3,4]
 
-#list에서 중복된 값 하나만 남기고 모두 제거
+#list에서 delarr에 있는 값들 제거
 for i in delarr:
     if i in array:
         array.remove(i)
 print(array)
+
