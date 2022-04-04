67d57b2b612156acbb72eb78664042bd8c7a1eff
eukjin <wjchang8028@naver.com>
2022-04-04 18:17:11 +0900

    .

---
+++ 프로그래머스/Lv2/lv2 방문길이.py
@@ -0,0 +1,19 @@
+def solution(dirs):
+    answer = 0
+    move = {'U':(0,1) , 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
+    movelist = set()
+    x,y = (0,0)
+
+    for i in dirs:
+        nx,ny = x + move[i][0] , y + move[i][1]
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            movelist.add((x,y,nx,ny))
+            movelist.add((nx,ny,x,y))
+            x,y = nx,ny 
+
+    answer = len(movelist) // 2
+    print(answer)
+    return answer
+
+solution("ULURRDLLU")
+solution("LULLLLLLU")
